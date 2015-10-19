5422b7ca34ceed73143fb913724417c636f9279f
Kate Fulton <katesuzannefulton@gmail.com>
2015-10-19 13:57:13 -0700

    resolve line discrepencies

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,6 @@ ActiveRecord::Schema.define(version: 20151019201651) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-<<<<<<< HEAD
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -38,7 +37,6 @@ ActiveRecord::Schema.define(version: 20151019201651) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-=======
   create_table "goals", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
@@ -48,6 +46,4 @@ ActiveRecord::Schema.define(version: 20151019201651) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
-
->>>>>>> ecb7c4ed0e313ca847c61905f7cdd5dfbb49f272
 end
