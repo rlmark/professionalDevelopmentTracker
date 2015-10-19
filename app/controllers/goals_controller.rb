4c4c559a09d8eee57b10b85f747cdbd52cd462e3
Kate Fulton <katesuzannefulton@gmail.com>
2015-10-19 14:20:40 -0700

    add filter to ensure only authenticated users can create goals

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,4 +1,5 @@
 class GoalsController < ApplicationController
+  before_filter :authenticate_user!, except: [:index, :show]
   before_action :set_goal, only: [:show, :edit, :update, :destroy]
 
   # GET /goals
