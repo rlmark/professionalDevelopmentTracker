ee03f7f53503429d0bbb4dae31ac7afb6c650e06
Rebecca Mark <rebecca.lmark@gmail.com>
2015-10-19 14:37:25 -0700

    due date functionality added.

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -70,6 +70,6 @@ class GoalsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def goal_params
-      params.require(:goal).permit(:name) 
+      params.require(:goal).permit(:name, :description, :due_date) 
     end
 end
