05aa486f797804e4b523a02f5b079525c25c6e08
Kate Fulton <katesuzannefulton@gmail.com>
2015-10-22 15:00:25 -0700

    work on specs and subtasks validations

--- spec/factories/subtasks.rb
+++ spec/factories/subtasks.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
   factory :subtask do
     name "MyString"
-goal_id 1
-description "MyText"
-status "MyString"
-due_date "2015-10-20"
+    goal_id 1
+    description "MyText"
+    status "MyString"
+    due_date "2015-10-20"
   end
 
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require 'spec_helper'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
+require 'capybara/rspec'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -50,4 +51,5 @@ RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   config.include Devise::TestHelpers, type: :controller
   config.include Warden::Test::Helpers
+  config.mock_with :mocha
 end
